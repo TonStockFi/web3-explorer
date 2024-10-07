959a4392dae4e706e84c8b34626f00b974be1d6b
TonStockFi <barry.ptp@gmail.com>
2024-10-07 11:23:04 +0630

    add web

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,237 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Account } from '@tonkeeper/core/dist/entries/account';
+import { localizationText } from '@tonkeeper/core/dist/entries/language';
+import { getApiConfig } from '@tonkeeper/core/dist/entries/network';
+import { WalletVersion } from '@tonkeeper/core/dist/entries/wallet';
+import { CopyNotification } from '@tonkeeper/uikit/dist/components/CopyNotification';
+import { FooterGlobalStyle } from '@tonkeeper/uikit/dist/components/Footer';
+import { HeaderGlobalStyle } from '@tonkeeper/uikit/dist/components/Header';
+import { DarkThemeContext } from '@tonkeeper/uikit/dist/components/Icon';
+import { GlobalListStyle } from '@tonkeeper/uikit/dist/components/List';
+import { Loading } from '@tonkeeper/uikit/dist/components/Loading';
+import { ModalsRoot } from '@tonkeeper/uikit/dist/components/ModalsRoot';
+import { SybHeaderGlobalStyle } from '@tonkeeper/uikit/dist/components/SubHeader';
+import { AmplitudeAnalyticsContext } from '@tonkeeper/uikit/dist/hooks/amplitude';
+import { AppContext, IAppContext } from '@tonkeeper/uikit/dist/hooks/appContext';
+import { AppSdkContext } from '@tonkeeper/uikit/dist/hooks/appSdk';
+import { useLock } from '@tonkeeper/uikit/dist/hooks/lock';
+import { StorageContext } from '@tonkeeper/uikit/dist/hooks/storage';
+import { I18nContext, TranslationContext, useTWithReplaces } from "@tonkeeper/uikit/dist/hooks/translation";
+import { UnlockNotification } from '@tonkeeper/uikit/dist/pages/home/UnlockNotification';
+import { UserThemeProvider } from '@tonkeeper/uikit/dist/providers/UserThemeProvider';
+import { useDevSettings } from '@tonkeeper/uikit/dist/state/dev';
+import { useUserFiatQuery } from '@tonkeeper/uikit/dist/state/fiat';
+import { useUserLanguage } from '@tonkeeper/uikit/dist/state/language';
+import { useProBackupState } from '@tonkeeper/uikit/dist/state/pro';
+import { useTonendpoint, useTonenpointConfig } from '@tonkeeper/uikit/dist/state/tonendpoint';
+import {
+    useAccountsState,
+    useAccountsStateQuery,
+    useActiveAccountQuery,
+    useActiveTonNetwork, useMutateActiveAccount
+} from "@tonkeeper/uikit/dist/state/wallet";
+import { GlobalStyle } from '@tonkeeper/uikit/dist/styles/globalStyle';
+import React, { FC, PropsWithChildren, Suspense, useEffect, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { MobileView } from './AppMobile';
+import { BrowserAppSdk } from './libs/appSdk';
+import { useAnalytics, useAppHeight, useLayout } from './libs/hooks';
+import { useGlobalPreferencesQuery } from "@tonkeeper/uikit/dist/state/global-preferences";
+import { useGlobalSetup } from "@tonkeeper/uikit/dist/state/globalSetup";
+import { useIsActiveAccountMultisig } from "@tonkeeper/uikit/dist/state/multisig";
+
+const QrScanner = React.lazy(() => import('@tonkeeper/uikit/dist/components/QrScanner'));
+const DesktopView = React.lazy(() => import('./AppDesktop'));
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 30000,
+            refetchOnWindowFocus: false
+        }
+    }
+});
+
+const sdk = new BrowserAppSdk();
+const TARGET_ENV = 'web';
+
+export const App: FC = () => {
+    return <RouterProvider router={router} />;
+};
+
+const Providers: FC<PropsWithChildren> = () => {
+  const { t: tSimple, i18n } = useTranslation();
+
+  const t = useTWithReplaces(tSimple);
+
+    const translation = useMemo(() => {
+        const languages = (import.meta.env.VITE_APP_LOCALES ?? 'en').split(',');
+        const client: I18nContext = {
+            t,
+            i18n: {
+                enable: true,
+                reloadResources: i18n.reloadResources,
+                changeLanguage: i18n.changeLanguage as any,
+                language: i18n.language,
+                languages: languages
+            }
+        };
+        return client;
+    }, [t, i18n]);
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Suspense>
+                <AppSdkContext.Provider value={sdk}>
+                    <TranslationContext.Provider value={translation}>
+                        <StorageContext.Provider value={sdk.storage}>
+                            <ThemeAndContent />
+                        </StorageContext.Provider>
+                    </TranslationContext.Provider>
+                </AppSdkContext.Provider>
+            </Suspense>
+        </QueryClientProvider>
+    );
+};
+
+const ThemeAndContent = () => {
+    const { data } = useProBackupState();
+    return (
+        <UserThemeProvider isPro={data?.valid} isProSupported>
+            <DarkThemeContext.Provider value={!data?.valid}>
+                <GlobalStyle />
+                <HeaderGlobalStyle />
+                <FooterGlobalStyle />
+                <SybHeaderGlobalStyle />
+                <GlobalListStyle />
+                <Loader />
+                <UnlockNotification sdk={sdk} />
+            </DarkThemeContext.Provider>
+        </UserThemeProvider>
+    );
+};
+
+const router = createBrowserRouter([
+    {
+        path: '/*',
+        element: <Providers />
+    }
+]);
+
+const Loader: FC = () => {
+    const network = useActiveTonNetwork();
+    const { data: activeAccount, isLoading: activeWalletLoading } = useActiveAccountQuery();
+    const { data: accounts, isLoading: isWalletsLoading } = useAccountsStateQuery();
+    const { data: lang, isLoading: isLangLoading } = useUserLanguage();
+    const { data: fiat } = useUserFiatQuery();
+    const { data: devSettings } = useDevSettings();
+    const { isLoading: globalPreferencesLoading } = useGlobalPreferencesQuery();
+    useGlobalSetup();
+
+    const [ios, standalone] = useMemo(() => {
+        return [sdk.isIOs(), sdk.isStandalone()] as const;
+    }, []);
+
+    const lock = useLock(sdk);
+    const { i18n } = useTranslation();
+
+    const tonendpoint = useTonendpoint({
+        targetEnv: TARGET_ENV,
+        build: sdk.version,
+        network,
+        lang
+    });
+    const { data: config } = useTonenpointConfig(tonendpoint);
+
+    useAppHeight();
+
+    const { data: tracker } = useAnalytics(activeAccount || undefined, accounts, sdk.version);
+
+    useEffect(() => {
+        if (activeAccount && lang && i18n.language !== localizationText(lang)) {
+            i18n.reloadResources([localizationText(lang)]).then(() =>
+                i18n.changeLanguage(localizationText(lang))
+            );
+        }
+    }, [activeAccount, i18n]);
+
+    const isMobile = useLayout();
+
+    if (
+        isWalletsLoading ||
+        activeWalletLoading ||
+        isLangLoading ||
+        config === undefined ||
+        lock === undefined ||
+        fiat === undefined ||
+        !devSettings ||
+      globalPreferencesLoading
+    ) {
+        return <Loading />;
+    }
+
+    const context: IAppContext = {
+        api: getApiConfig(config, network, import.meta.env.VITE_APP_TONCONSOLE_HOST),
+        fiat,
+        config,
+        tonendpoint,
+        standalone,
+        extension: false,
+        proFeatures: true,
+        ios,
+        defaultWalletVersion: WalletVersion.V5R1,
+        hideMultisig: isMobile,
+        env: {
+            tgAuthBotId: import.meta.env.VITE_APP_TG_BOT_ID,
+            stonfiReferralAddress: import.meta.env.VITE_APP_STONFI_REFERRAL_ADDRESS
+        }
+    };
+
+    return (
+        <AmplitudeAnalyticsContext.Provider value={tracker}>
+            <AppContext.Provider value={context}>
+                <Content
+                    activeAccount={activeAccount}
+                    lock={lock}
+                    standalone={standalone}
+                />
+                <CopyNotification hideSimpleCopyNotifications={!standalone} />
+                <Suspense>
+                    <QrScanner />
+                </Suspense>
+                <ModalsRoot />
+            </AppContext.Provider>
+        </AmplitudeAnalyticsContext.Provider>
+    );
+};
+
+const Content: FC<{
+    activeAccount?: Account | null;
+    lock: boolean;
+    standalone: boolean;
+}> = ({ activeAccount, lock, standalone }) => {
+    const isMobile = useLayout();
+    const accounts = useAccountsState();
+    const isActiveMultisig = useIsActiveAccountMultisig();
+    const { mutate: setActiveAccount } = useMutateActiveAccount();
+
+    useEffect(() => {
+        if (isMobile && isActiveMultisig) {
+            const firstNotMultisig = accounts.filter(a => a.type !== 'ton-multisig')[0];
+            if (firstNotMultisig) {
+                setActiveAccount(firstNotMultisig.id)
+            }
+        }
+    }, [isMobile, isActiveMultisig, setActiveAccount]);
+
+    if (isMobile) {
+        return <MobileView activeAccount={activeAccount} lock={lock} standalone={standalone} />;
+    } else {
+        return (
+            <Suspense fallback={<Loading />}>
+                <DesktopView activeAccount={activeAccount} lock={lock} />
+            </Suspense>
+        );
+    }
+};
---
+++ apps/web/src/libs/hooks.ts
@@ -0,0 +1,116 @@
+import { useQuery } from '@tanstack/react-query';
+import { Account } from '@tonkeeper/core/dist/entries/account';
+import { throttle } from '@tonkeeper/core/dist/utils/common';
+import { Analytics, AnalyticsGroup, toWalletType } from '@tonkeeper/uikit/dist/hooks/analytics';
+import { AptabaseWeb } from '@tonkeeper/uikit/dist/hooks/analytics/aptabase-web';
+import { Gtag } from '@tonkeeper/uikit/dist/hooks/analytics/gtag';
+import { QueryKey } from '@tonkeeper/uikit/dist/libs/queryKey';
+import { AppRoute } from '@tonkeeper/uikit/dist/libs/routes';
+import { useActiveTonNetwork } from '@tonkeeper/uikit/dist/state/wallet';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export const useAppHeight = () => {
+    useEffect(() => {
+        const appHeight = throttle(() => {
+            const doc = document.documentElement;
+            doc.style.setProperty('--app-height', `${window.innerHeight}px`);
+        }, 50);
+        window.addEventListener('resize', appHeight);
+        appHeight();
+
+        return () => {
+            window.removeEventListener('resize', appHeight);
+        };
+    }, []);
+};
+
+export const useAppWidth = (standalone: boolean) => {
+    useEffect(() => {
+        const appWidth = throttle(() => {
+            if (standalone) {
+                const doc = document.documentElement;
+                doc.style.setProperty('--app-width', `${window.innerWidth}px`);
+            } else {
+                const doc = document.documentElement;
+                const app = (document.getElementById('root') as HTMLDivElement).childNodes.item(
+                    0
+                ) as HTMLDivElement;
+
+                doc.style.setProperty('--app-width', `${app.clientWidth}px`);
+            }
+        }, 50);
+        window.addEventListener('resize', appWidth);
+
+        appWidth();
+
+        return () => {
+            window.removeEventListener('resize', appWidth);
+        };
+    }, [standalone]);
+};
+
+export const useAnalytics = (activeAccount?: Account, accounts?: Account[], version?: string) => {
+    const network = useActiveTonNetwork();
+    return useQuery<Analytics>(
+        [QueryKey.analytics, network],
+        async () => {
+            const tracker = new AnalyticsGroup(
+                new AptabaseWeb(
+                    import.meta.env.VITE_APP_APTABASE_HOST,
+                    import.meta.env.VITE_APP_APTABASE,
+                    version
+                ),
+                new Gtag(import.meta.env.VITE_APP_MEASUREMENT_ID)
+            );
+
+            tracker.init({
+                application: 'Web',
+                walletType: toWalletType(activeAccount?.activeTonWallet),
+                activeAccount: activeAccount!,
+                accounts: accounts!,
+                network
+            });
+
+            return tracker;
+        },
+        { enabled: accounts != null && activeAccount !== undefined }
+    );
+};
+
+export const useLayout = () => {
+    const navigate = useNavigate();
+
+    const [isMobile, setMobile] = useState(localStorage.getItem('layout') === 'true');
+
+    useEffect(() => {
+        const appWidth = throttle(() => {
+            if (window.innerWidth >= 1024) {
+                setMobile(old => {
+                    if (old !== false) {
+                        navigate(AppRoute.home);
+                        localStorage.setItem('layout', 'false');
+                    }
+                    return false;
+                });
+            } else {
+                setMobile(old => {
+                    if (old !== true) {
+                        navigate(AppRoute.home);
+                        localStorage.setItem('layout', 'true');
+                    }
+                    return true;
+                });
+            }
+        }, 50);
+
+        window.addEventListener('resize', appWidth);
+
+        appWidth();
+
+        return () => {
+            window.removeEventListener('resize', appWidth);
+        };
+    }, [navigate, setMobile]);
+    return isMobile;
+};
